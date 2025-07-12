e792be48d3c19289fbdc959327c78352261a62a4
Mujahid Anwar <111413266+mujahid141@users.noreply.github.com>
2025-07-12 16:14:31 +0500

    debug

--- App.js
+++ App.js
@@ -3,9 +3,25 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { AuthProvider } from "./src/context/AuthContext";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { initBaseUrl } from "./src/utils/sharesUtils";
 const Stack = createStackNavigator();
 
+
+import  { useEffect, useState } from 'react';
+
 export default function App() {
+    const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      await initBaseUrl(); // ✅ Load base URL from AsyncStorage
+      setReady(true);
+    };
+    init();
+  }, []);
+
+
+
   return (
     <AuthProvider>
       <NavigationContainer>
